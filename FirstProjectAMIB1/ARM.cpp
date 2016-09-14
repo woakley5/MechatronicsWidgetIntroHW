3dd9327f1ac7fc4f27f6ffbc9cc94f45c4f0d6a7
Will Oakley <woakley5@icloud.com>
2016-09-13 19:09:42 -0700

    Minor formatting changes

--- FirstProjectAMIB1/ARM.cpp
+++ FirstProjectAMIB1/ARM.cpp
@@ -3,113 +3,111 @@
 #include "states.h"
 
 namespace ARM{
+  const byte SHORT_SENSOR = 27;
+  const byte TALL_SENSOR = 26;
+  const byte ROTATION_SENSOR = 29;
+  const byte ELECTROMAGNET = 28;
+  SpeedyStepper rotationMotor;
+  RCServo electromagnet;
+  const byte AIR = 3;
+  bool moving = false;
+  //values::rotationPosition;
 
-const byte SHORT_SENSOR = 27;
-const byte TALL_SENSOR = 26;
-const byte ROTATION_SENSOR = 29;
-const byte ELECTROMAGNET = 28;
-SpeedyStepper rotationMotor;
-RCServo electromagnet;
-const byte AIR = 3;
-bool moving = false;
-//values::rotationPosition;
+  void setup() {
+    pinMode(AIR, OUTPUT);
+    rotationMotor.connectToPort(2);
+    rotationMotor.setStepsPerRevolution(3200);
+    rotationMotor.setSpeedInRevolutionsPerSecond(0.15);
+    rotationMotor.setAccelerationInRevolutionsPerSecondPerSecond(1);
+    rotationMotor.disableStepper();
+    electromagnet.connectToPin(ELECTROMAGNET, 1000, 2000, 0.0);
+    pinMode(SHORT_SENSOR, INPUT_PULLUP);
+    pinMode(TALL_SENSOR, INPUT_PULLUP);
+    pinMode(ROTATION_SENSOR, INPUT_PULLUP);
+    events::resetArmPosition();
+  }
 
-void setup() {
-  pinMode(AIR, OUTPUT);
-  rotationMotor.connectToPort(2);
-  rotationMotor.setStepsPerRevolution(3200);
-  rotationMotor.setSpeedInRevolutionsPerSecond(0.15);
-  rotationMotor.setAccelerationInRevolutionsPerSecondPerSecond(1);
-  rotationMotor.disableStepper();
-  electromagnet.connectToPin(ELECTROMAGNET, 1000, 2000, 0.0); 
-  pinMode(SHORT_SENSOR, INPUT_PULLUP);  
-  pinMode(TALL_SENSOR, INPUT_PULLUP);  
-  pinMode(ROTATION_SENSOR, INPUT_PULLUP);
-  events::resetArmPosition(); 
-}
+  void loop() {
 
-void loop() {
-  
- }
+   }
 
-void events::moveFromTallToShort()
-{
+  void events::moveFromTallToShort()
+  {
+      moving = true;
+      rotationMotor.moveToPositionInRevolutions(0.24);
+      events::lowerArm();
+      events::enableElectromagnet();
+      delay(1500);
+      events::raiseArm();
+      delay(1500);
+      rotationMotor.moveToPositionInRevolutions(0.41);
+      delay(1000);
+      events::lowerArm();
+      delay(1500);
+      events::disableElectromagnet();
+      delay(1500);
+      events::raiseArm();
+      delay(1500);
+      rotationMotor.moveToPositionInRevolutions(0);
+      delay(1500);
+      moving = false;
+
+  }
+
+  void events::moveFromShortToTall()
+  {
     moving = true;
-    rotationMotor.moveToPositionInRevolutions(0.24);
+    rotationMotor.moveToPositionInRevolutions(0.41);
     events::lowerArm();
     events::enableElectromagnet();
     delay(1500);
     events::raiseArm();
     delay(1500);
-    rotationMotor.moveToPositionInRevolutions(0.41);
+    rotationMotor.moveToPositionInRevolutions(0.24);
     delay(1000);
     events::lowerArm();
     delay(1500);
     events::disableElectromagnet();
-    delay(1500);
     events::raiseArm();
-    delay(1500);
     rotationMotor.moveToPositionInRevolutions(0);
     delay(1500);
     moving = false;
-  
-}
-
-void events::moveFromShortToTall()
-{
-  moving = true;
-  rotationMotor.moveToPositionInRevolutions(0.41);
-  events::lowerArm();
-  events::enableElectromagnet();
-  delay(1500);
-  events::raiseArm();
-  delay(1500);
-  rotationMotor.moveToPositionInRevolutions(0.24);
-  delay(1000);
-  events::lowerArm();
-  delay(1500);
-  events::disableElectromagnet();
-  events::raiseArm();
-  rotationMotor.moveToPositionInRevolutions(0);
-  delay(1500);
-  moving = false;
-}
-
-void events::disableElectromagnet()
-{
-  electromagnet.setServoPosition(0);
-}
-
-void events::enableElectromagnet()
-{
-  electromagnet.setServoPosition(-1);
-}
-
-void events::moveArm(){
-  rotationMotor.enableStepper();
-  float rot = rotations.value;
-  rotationMotor.setupMoveInRevolutions(rot/100);
-  while(!rotationMotor.motionComplete())
-  {
-	  rotationMotor.processMovement();
   }
-  //rotationMotor.processMovement();
-}
 
-void events::lowerArm()
-{
-  digitalWrite(AIR, HIGH);
-}
+  void events::disableElectromagnet()
+  {
+    electromagnet.setServoPosition(0);
+  }
 
-void events::raiseArm()
-{
-  digitalWrite(AIR, LOW);
-}
+  void events::enableElectromagnet()
+  {
+    electromagnet.setServoPosition(-1);
+  }
 
-void events::resetArmPosition()
-{
-  rotationMotor.enableStepper();
-  rotationMotor.moveToHomeInRevolutions(-1, 0.15, 2, ROTATION_SENSOR);
-}
-}
+  void events::moveArm(){
+    rotationMotor.enableStepper();
+    float rot = rotations.value;
+    rotationMotor.setupMoveInRevolutions(rot/100);
+    while(!rotationMotor.motionComplete())
+    {
+  	  rotationMotor.processMovement();
+    }
+    //rotationMotor.processMovement();
+  }
 
+  void events::lowerArm()
+  {
+    digitalWrite(AIR, HIGH);
+  }
+
+  void events::raiseArm()
+  {
+    digitalWrite(AIR, LOW);
+  }
+
+  void events::resetArmPosition()
+  {
+    rotationMotor.enableStepper();
+    rotationMotor.moveToHomeInRevolutions(-1, 0.15, 2, ROTATION_SENSOR);
+  }
+}
--- FirstProjectAMIB1/MOTIONMACHINE.cpp
+++ FirstProjectAMIB1/MOTIONMACHINE.cpp
@@ -3,79 +3,78 @@
 #include "states.h"
 
 namespace MOTIONMACHINE {
-SpeedyStepper stepper1;
-RCServo gateServo;
-const byte LOWER_SENSOR = 59;
-const byte UPPER_SENSOR = 60;
-const byte LIMIT_SENSOR = 61;
-const byte STEP_MOTOR = 4;
-const byte SERVO_GATE = 58;
-bool liftReady = false;
-int servoClosedPosition;
-int servoOpenPosition;
+  SpeedyStepper stepper1;
+  RCServo gateServo;
+  const byte LOWER_SENSOR = 59;
+  const byte UPPER_SENSOR = 60;
+  const byte LIMIT_SENSOR = 61;
+  const byte STEP_MOTOR = 4;
+  const byte SERVO_GATE = 58;
+  bool liftReady = false;
+  int servoClosedPosition;
+  int servoOpenPosition;
 
-void setup() {
-  stepper1.connectToPort(1);
-  stepper1.setStepsPerMillimeter(100);
-  stepper1.setSpeedInMillimetersPerSecond(25);
-  stepper1.setAccelerationInMillimetersPerSecondPerSecond(10);
-  stepper1.disableStepper();
-  //gateServo.setServoPosition(25);
-  gateServo.connectToPin(SERVO_GATE);
-  pinMode(STEP_MOTOR, OUTPUT);
-  pinMode(LOWER_SENSOR, INPUT_PULLUP);
-  pinMode(UPPER_SENSOR, INPUT_PULLUP);
-  pinMode(LIMIT_SENSOR, INPUT_PULLUP);
-  servoClosedPosition = 0;
-  servoOpenPosition = 0.9;
-  events::setLiftToZero();
-}
-
-void loop() {
-
-}
-
-void events::moveLiftUp() {
-  liftReady = false;
-  gateServo.setServoPosition(servoClosedPosition);
-
-  stepper1.enableStepper();
-  stepper1.setupMoveInMillimeters(-226);
-  while(!stepper1.motionComplete())
-  {
-    stepper1.processMovement();
+  void setup() {
+    stepper1.connectToPort(1);
+    stepper1.setStepsPerMillimeter(100);
+    stepper1.setSpeedInMillimetersPerSecond(25);
+    stepper1.setAccelerationInMillimetersPerSecondPerSecond(10);
+    stepper1.disableStepper();
+    //gateServo.setServoPosition(25);
+    gateServo.connectToPin(SERVO_GATE);
+    pinMode(STEP_MOTOR, OUTPUT);
+    pinMode(LOWER_SENSOR, INPUT_PULLUP);
+    pinMode(UPPER_SENSOR, INPUT_PULLUP);
+    pinMode(LIMIT_SENSOR, INPUT_PULLUP);
+    servoClosedPosition = 0;
+    servoOpenPosition = 0.9;
+    events::setLiftToZero();
   }
-  stepper1.disableStepper();
 
-}
+  void loop() {
 
-void events::moveToBottom() {
-  stepper1.enableStepper();
-  stepper1.setupMoveInMillimeters(0);
-  while(!stepper1.motionComplete())
-  {
-    stepper1.processMovement();
-  }  
-  stepper1.disableStepper();
-  liftReady = true;
+  }
 
-  gateServo.setServoPosition(servoOpenPosition);
-}
+  void events::moveLiftUp() {
+    liftReady = false;
+    gateServo.setServoPosition(servoClosedPosition);
 
-void events::setLiftToZero() {
-  stepper1.enableStepper();
-  stepper1.moveToHomeInMillimeters(1, 50, 250, LIMIT_SENSOR);
-  stepper1.setCurrentPositionInMillimeters(0);
-  stepper1.disableStepper();
-  liftReady = true;
-  gateServo.setServoPosition(servoOpenPosition);
-}
+    stepper1.enableStepper();
+    stepper1.setupMoveInMillimeters(-226);
+    while(!stepper1.motionComplete())
+    {
+      stepper1.processMovement();
+    }
+    stepper1.disableStepper();
 
-void events::runSteps(){
-  digitalWrite(STEP_MOTOR, HIGH);
-}
-void events::stopSteps(){
-  digitalWrite(STEP_MOTOR, LOW);
-}
-}
+  }
 
+  void events::moveToBottom() {
+    stepper1.enableStepper();
+    stepper1.setupMoveInMillimeters(0);
+    while(!stepper1.motionComplete())
+    {
+      stepper1.processMovement();
+    }
+    stepper1.disableStepper();
+    liftReady = true;
+
+    gateServo.setServoPosition(servoOpenPosition);
+  }
+
+  void events::setLiftToZero() {
+    stepper1.enableStepper();
+    stepper1.moveToHomeInMillimeters(1, 50, 250, LIMIT_SENSOR);
+    stepper1.setCurrentPositionInMillimeters(0);
+    stepper1.disableStepper();
+    liftReady = true;
+    gateServo.setServoPosition(servoOpenPosition);
+  }
+
+  void events::runSteps(){
+    digitalWrite(STEP_MOTOR, HIGH);
+  }
+  void events::stopSteps(){
+    digitalWrite(STEP_MOTOR, LOW);
+  }
+}
